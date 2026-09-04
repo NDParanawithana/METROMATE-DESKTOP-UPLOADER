8ea0ceb46bb9816521148bfa816462642da2548c
Darshana-stan <standarshan@gmail.com>
2026-09-04 13:13:49 +0530

    feat: initialize Electron desktop stem uploader application structure with UI styles

--- forge.config.js
+++ forge.config.js
@@ -32,6 +32,7 @@ module.exports = {
     {
       name: '@electron-forge/plugin-webpack',
       config: {
+        port: 3001,
         mainConfig: './webpack.main.config.js',
         renderer: {
           config: './webpack.renderer.config.js',
